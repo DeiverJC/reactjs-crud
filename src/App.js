8951ae755029a973d8f995cde8c44bbb5c26e9e7
deiverjc <deiver-jc@live.com>
2021-03-21 16:59:11 -0500

    Added delete task

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ function App() {
     setTasks([...tasks, newTask])
     setTask("")
   }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id ))
+  }
+
   return (
     <div className="container mt-5">
       <h1>Tasks</h1>
@@ -25,17 +30,32 @@ function App() {
       <div className="row">
         <div className="col-8">
           <h4 className="text-center">Tasks list</h4>
-          <ul className="list-group">
-          {
-            tasks.map(({id, task}) => (
-              <li className="list-group-item" key={id}>
-                <span className="lead">{task}</span>
-                <button className="btn btn-danger btn-sm float-right">Delete</button>
-                <button className="btn btn-warning btn-sm float-right mr-2">Edit</button>
-              </li>
-            ))
-          }
-          </ul>
+          {isEmpty(tasks) ? (
+            <h5 className="text-center mt-5">No hay tareas para mostrar</h5>
+          ) : (
+            <ul className="list-group">
+            {
+              tasks.map(({id, task}) => (
+                <li className="list-group-item" key={id}>
+                  <span className="lead">{task}</span>
+                  <div className="float-right">
+                    <button 
+                      className="btn btn-warning btn-sm mr-2"
+                    >
+                      Edit
+                    </button>
+                    <button 
+                      className="btn btn-danger btn-sm"
+                      onClick={() => { deleteTask(id) }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))
+            }
+            </ul>
+          )}
         </div>
         <div className="col-4">
           <h4 className="text-center">Create task</h4>
