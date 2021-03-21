558cfd01324e88f203a2e9549d2a83b88efe0210
deiverjc <deiver-jc@live.com>
2021-03-21 15:56:51 -0500

    added state for create task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+import { isEmpty } from 'lodash'
+import React, { useState } from 'react'
+
 function App() {
+  const [task, setTask] = useState("")
+  const addTask = (e) => {
+    e.preventDefault()
+    if(isEmpty(task)) {
+      console.log('task is empty')
+      return
+    }
+    console.log('ok', task)
+    setTask("")
+  }
   return (
     <div className="container mt-5">
       <h1>Tasks</h1>
@@ -16,8 +29,14 @@ function App() {
         </div>
         <div className="col-4">
           <h4 className="text-center">Create task</h4>
-          <form>
-            <input type="text" className="form-control mb-2" placeholder="Type task..."/>
+          <form onSubmit={addTask}>
+            <input 
+              type="text" 
+              className="form-control mb-2" 
+              placeholder="Type task..."
+              onChange={(text) => {setTask(text.target.value)}}
+              value={task}
+            />
             <button className="btn btn-dark btn-block">Add</button>
           </form>
         </div>
