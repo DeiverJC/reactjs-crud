ea4ad2f601d6c4354cfefa5bf7c209f838ae29e9
deiverjc <deiver-jc@live.com>
2021-03-21 15:18:32 -0500

    Add tasks list and create task form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ function App() {
     <div className="container mt-5">
       <h1>Tasks</h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Tasks list</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Task name</span>
+              <button className="btn btn-danger btn-sm float-right">Delete</button>
+              <button className="btn btn-warning btn-sm float-right mr-2">Edit</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Create task</h4>
+          <form>
+            <input type="text" className="form-control mb-2" placeholder="Type task..."/>
+            <button className="btn btn-dark btn-block">Add</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
